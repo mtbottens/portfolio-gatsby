d7e4ff1bd1b600dc9bbfc51718a712d3c9e8fd12
Michael Bottens <mbottens@gorillagroup.com>
2018-02-28 17:43:58 -0500

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,16 @@ module.exports = {
         accessToken: `${process.env.GATSBY_DIRECTUS_ACCESS_TOKEN}`
       }
     },
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js'
+      }
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp'
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sass'
   ],
 };
